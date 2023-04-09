b218ea4ab1117e3cb092a1362e81c35c2e22670a
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-04-09 21:06:34 +0200

    encode and decode functions

--- GenerateNumbers.py
+++ GenerateNumbers.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def isPrime(num):
     for n in range(2,int(num**0.5)+1):
@@ -8,9 +9,11 @@ def isPrime(num):
 
 def generateRandomPrimeNumber():
     while(True):
-        num=random.randint(0,(2**2048)-1)
+        num=random.randint(0,(2**35)-1)
         if(isPrime(num)):
             break
+    print("5lsna")    
+    return num    
 
 def GCD(number1,number2):
     if(number2==0):
@@ -24,5 +27,36 @@ def GenerateE(PhiN):
             break
 
 def GenerateD(E,PhiN):
-    return pow(E,-1,PhiN)        
+    return pow(E,-1,PhiN) 
+
+def convCharToNum(c):
+    if(ord(c)>=97 and ord(c)<=122):
+        return ord(c)-87
+    if(ord(c)>=48 and ord(c)<=57):
+        return ord(c)-48
+    return 36
+
+def convNumToChar(num):
+    if(num>=10 and num <=35):
+        return ord('a')+num-10
+    if(num>=0 and num <=9):
+        return ord('0')+num
+    return 32
+
+def encode(s):
+    counter=0
+    for i in range(5):
+        charValue=convCharToNum(s[i])
+        print(charValue)
+        counter+= charValue*pow(37,4-i)
+    return counter  
+
+def decode(s):
+    group="" 
+    for i in range(5):
+        value=int(s/(pow(37,4-i)))
+        group+=chr(convNumToChar(value))
+        s=s%(pow(37,4-i))
+    return group 
+
 
